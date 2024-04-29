f3a7dc4259b6beea47f21fd5c8a48992001e721e
dongkyusq <hasd789@naver.com>
2024-04-29 20:40:25 +0900

    save

--- MOMENTUM LOGIN/app.js
+++ MOMENTUM LOGIN/app.js
@@ -1,22 +1,38 @@
 const loginForm = document.querySelector("#login-form");
 const loginInput = document.querySelector("#login-form input");
-const link = document.querySelector("a");
 const greeting = document.querySelector("#greeting");
 
-const HIDDEN_CLASSNAME = "hidden";
+// const link = document.querySelector("a");
 
+const HIDDEN_CLASSNAME = "hidden";
+const USERNAME_KEY = "username";
 
 function onLoginSubmit(event){          //실행 될때 event object를 argument로 줌
     event.preventDefault();            //새로고침 되는거 막음
     loginForm.classList.add(HIDDEN_CLASSNAME);
     const username = loginInput.value;
+    localStorage.setItem(USERNAME_KEY, username);
+    paintGreetings(username);
+}
+
+function paintGreetings(username){
     greeting.innerText = `Hello ${username}`;
     greeting.classList.remove(HIDDEN_CLASSNAME);
 }
 
-function handleLink(event){
-    event.preventDefault();
-}
 
-loginForm.addEventListener("submit", onLoginSubmit);
-link.addEventListener("click", handleLink);
+const savedUserName = localStorage.getItem(USERNAME_KEY);
+
+if(savedUserName === null) {
+    loginForm.classList.remove(HIDDEN_CLASSNAME);
+    loginForm.addEventListener("submit", onLoginSubmit);
+} else {
+    paintGreetings(savedUserName);
+}; 
+
+
+// function handleLink(event){
+//     event.preventDefault();
+// }
+
+// link.addEventListener("click", handleLink);
